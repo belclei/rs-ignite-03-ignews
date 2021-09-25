7402acb2289f870b5370ce494cb2c00089f68b8b
belclei <belclei@gmail.com>
2021-09-25 18:02:27 -0300

    global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,12 @@
-import styles from '../styles/home.module.scss'
+import Head from 'next/head'
+
 export default function Home() {
-  return <h1 className={styles.title}>Olá Enfermeira!</h1>
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <h1>Olá Enfermeira!</h1>
+    </>
+  )
 }
